415593cd7bd675fd0719b19416b5a7662df3b410
steve-zxc <zxc.steve@gmail.com>
2021-10-10 16:18:13 +0800

    modify path to /zxc-steve.github.io/a2hs/

--- a2hs/a2hs/index.js
+++ a2hs/a2hs/index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+      .register('/zxc-steve.github.io/a2hs/a2hs/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
