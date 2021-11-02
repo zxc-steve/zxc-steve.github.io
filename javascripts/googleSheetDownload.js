dc20d5cbe7920a49176cfd96900376a7e7dff1b0
steve-zxc <zxc.steve@gmail.com>
2021-11-02 11:26:18 +0800

    Add project files.

---
+++ javascripts/googleSheetDownload.js
@@ -0,0 +1,34 @@
+let info = document.getElementById("info");
+let info1 = document.getElementById("info1");
+
+func1();
+async function func1() {
+    //let url = "https://script.google.com/macros/s/AKfycbzisNeXh3VShodCjCp1qwIFQiVAj3_qe3LpLiGyLn0sF9JnN6AbBixFODRbMt_wU4qszA/exec"
+    let url ="https://script.google.com/macros/s/AKfycbyzPWep7sKWV8B8BVBzs-L5BNtXssO6gF2raLL2zV4pow5VnDzYMh47su02fp6TVOxg7A/exec"
+    let res = await fetch(url, {
+        //headers: { 'Content-Type': 'text/plain' },
+        //redirect: 'follow',
+        //mode: 'no-cors' // 'cors' by default, this will cause problem in google sheet API
+    })
+    //info.innerText = await res.text();
+    info.innerText = JSON.stringify(await res.json());
+    let cnt = 30;
+    while (cnt > 0) {
+        info1.innerText = `Will download google sheet in ${cnt-1} seconds`;
+        await sleep(1000);
+        cnt--;
+    }
+    let downloadUrl ="https://docs.google.com/spreadsheets/d/e/2PACX-1vTF8VAfzBNOPxosU_PPtiZhy-HlUMQmlDdej1qhOHeppHUUXwubFamKIF-9nuThSqChJ3TbwUb2ZPBI/pub?output=xlsx"
+    download(downloadUrl)
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function download(dataurl) {
+    const link = document.createElement("a");
+    link.href = dataurl;
+    link.click();
+}
+//let url =""
+//fetch(url);
